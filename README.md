7b57f8107ed21c87f2762d55561f903f906e74d3
antoniofdez76 <jose.fernandez@udgvirtual.udg.mx>
2023-03-24 14:09:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_movies_namer
+ 3.3. Inicio del desarrollo
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+          //child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
