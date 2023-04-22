5823d3fb3a55060d47f34c48216a838cd7b75947
antoniofdez76 <jose.fernandez@udgvirtual.udg.mx>
2023-04-21 21:57:03 -0600

    3.5. Home screen
    3.5. Home screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,102 @@ class Movie {
   Movie(this.title, this.image, this.description);
 }
 
-class MyApp extends StatelessWidget {
-  final List<Movie> movies = [
-    Movie(
-        'Pelicula 1',
-        'https://picsum.photos/id/5/200/300',
-        'Descripción.'
-    ),
-    Movie(
-        'Pelicula 2',
-        'https://picsum.photos/id/2/200/300',
-        'Descripción.'
-    ),
-    Movie(
-        'Pelicula 3',
-        'https://picsum.photos/id/3/200/300',
-        'Descripción.'
-    ),
-  ];
+class MovieListPage extends StatelessWidget {
+  final List<Movie> movies;
 
+  const MovieListPage({required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catálogo de Películas'),
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.all(10.0),
+            height: 150.0,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.network(
+                    movies[index].image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          movies[index].title,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          movies[index].description,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  final List<Movie> movies = [    Movie(      'Pelicula 1',      'https://picsum.photos/id/5/200/300',      'Descripción.',    ),    Movie(      'Pelicula 2',      'https://picsum.photos/id/2/200/300',      'Descripción.',    ),    Movie(      'Pelicula 3',      'https://picsum.photos/id/3/200/300',      'Descripción.',    ),  ];
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World'),
+            Image.network(
+              'https://picsum.photos/id/870/200/300?grayscale&blur=2',
+              fit: BoxFit.cover,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MovieListPage(
+                      movies: movies,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Ver películas'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,55 +115,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Catálogo de Películas'),
-        ),
-        body: ListView.builder(
-          itemCount: movies.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: EdgeInsets.all(10.0),
-              height: 150.0,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Image.network(
-                      movies[index].image,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      margin: EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            movies[index].title,
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 10.0),
-                          Text(
-                            movies[index].description,
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
+      home: WelcomePage(),
     );
   }
 }
