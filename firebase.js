40e2d909f48316d11cd754f0e4acd31da23cca71
antoniofdez76 <jose.fernandez@udgvirtual.udg.mx>
2023-05-08 21:38:54 -0600

    3.7. Integración de base de datos
    3.7. Integración de base de datos

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Inicializa Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBy_VOJ0SlCY7-FbYAiOe7jplHuKlHRs5A",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "appfluttermobile",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
